f312dd21d768644063e7ccd4a1c9f39b06608b28
Troels Michael Trebbien <gatman@gatman.dk>
2012-07-17 17:07:15 +0200

    Small changes to Chien search and a dual port lookup table.

--- chien_search.vhd
+++ chien_search.vhd
@@ -3,6 +3,7 @@
 -----------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 library work;
 use work.vhdlib_package.all;
@@ -27,17 +28,26 @@ architecture rtl of chien_search is
   constant M  : integer := GF_POLYNOMIAL'length-1;
 
   subtype gf_elem is std_logic_vector(M-1 downto 0);
-  type gf_array_desc_t is array(CORRECTABLE_ERR downto 0) of gf_elem;
+  type gf_array_desc_t  is array(CORRECTABLE_ERR downto 0) of gf_elem;
+  type gf_err_vals      is array(CORRECTABLE_ERR-1 downto 0) of gf_elem;
   type calculator_state_t is (IDLE, CALCULATING);
 
   constant GF_ZERO        : gf_elem := (OTHERS => '0');
+  constant GF_MAX         : gf_elem := (OTHERS => '1');
 
   signal gammas           : gf_array_desc_t;
   signal gammas_new       : gf_array_desc_t;
   signal gammas_sum       : gf_elem;
-  signal n                : integer range 0 to 2**M-2;
+  signal err_roots        : gf_err_vals;
+  signal err_locations    : gf_err_vals;
+  signal k                : integer range 0 to CORRECTABLE_ERR-1;
+  signal n                : unsigned(M-1 downto 0);
   signal calculator_state : calculator_state_t;
-  signal debug_root_found : std_logic;
+  signal root_found       : std_logic;
+  signal gf_elem_exp_in   : gf_elem;
+  signal gf_elem_inv_in   : gf_elem;
+  signal gf_elem_exp_out  : gf_elem;
+  signal gf_elem_inv_out  : gf_elem;
 
 begin
 
@@ -55,21 +65,45 @@ begin
       );
   end generate gen_coef_multipliers;
 
+  -- lookup table
+  lookup_table : entity work.gf_lookup_table_dp(rtl)
+    generic map (
+      GF_POLYNOMIAL   => GF_POLYNOMIAL,
+      TABLE_TYPE      => EXP_TABLE_TYPE
+    )
+    port map (
+      clk             => clk,
+      elem_in_a       => gf_elem_exp_in,
+      elem_in_b       => gf_elem_inv_in,
+      elem_out_a      => gf_elem_exp_out,
+      elem_out_b      => gf_elem_inv_out
+    );
+
+  gf_elem_exp_in  <= std_logic_vector(n);
+  gf_elem_inv_in  <= GF_MAX xor std_logic_vector(n);
+
   clk_proc : process (clk, rst)
   begin
     if rst = '1' then
       gammas            <= (OTHERS => GF_ZERO);
-      n                 <= 0;
+      err_roots         <= (OTHERS => GF_ZERO);
+      err_locations     <= (OTHERS => GF_ZERO);
+      k                 <= 0;
+      n                 <= (OTHERS => '0');
       calculator_state  <= IDLE;
       ready             <= '0';
-      debug_root_found  <= '0';
+      root_found        <= '0';
     elsif rising_edge(clk) then
-      ready             <= '0'; -- preassignment
-      debug_root_found  <= '0';
+      -- preassignments
+      ready             <= '0';
+      root_found        <= '0';
 
       if new_calc = '1' then
-        n                 <= 0;
+        k                 <= 0;
+        n                 <= (OTHERS => '0');
         calculator_state  <= CALCULATING;
+        err_roots         <= (OTHERS => GF_ZERO);
+        err_locations     <= (OTHERS => GF_ZERO);
         for i in CORRECTABLE_ERR downto 0 loop
           gammas(i) <= err_locator_in((i+1)*M-1 downto i*M);
         end loop;
@@ -84,7 +118,7 @@ begin
 
         if gammas_sum = GF_ZERO then
           -- alpha^n is a root of the error locator
-          debug_root_found  <= '1';
+          root_found        <= '1';
         end if;
 
         -- set new gamma values
@@ -93,8 +127,18 @@ begin
         if calculator_state = IDLE then
           ready <= '1';
           -- output calculated values
+          -- TODO: Output one cycle later than expected
         end if;
       end if;
+
+      if root_found = '1' then
+        err_roots(k)      <= gf_elem_exp_out;
+        err_locations(k)  <= gf_elem_inv_out;
+        if k < CORRECTABLE_ERR-1 then
+          k               <= k + 1;
+        end if;
+      end if;
+
     end if;
   end process clk_proc;
 
---
+++ gf_lookup_table_dp.vhd
@@ -0,0 +1,84 @@
+--------------------------------------------
+-- GF(2^M) element lookup table dual port --
+--------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.vhdlib_package.all;
+
+entity gf_lookup_table_dp is
+  generic (
+    GF_POLYNOMIAL   : std_logic_vector := G709_GF_POLY; -- irreducible, binary polynomial
+    TABLE_TYPE      : string           := INV_TABLE_TYPE
+  );
+  port (
+    clk             : in  std_logic;
+    elem_in_a       : in  std_logic_vector(GF_POLYNOMIAL'length-2 downto 0);
+    elem_in_b       : in  std_logic_vector(GF_POLYNOMIAL'length-2 downto 0);
+    elem_out_a      : out std_logic_vector(GF_POLYNOMIAL'length-2 downto 0);
+    elem_out_b      : out std_logic_vector(GF_POLYNOMIAL'length-2 downto 0)
+  );
+
+end entity;
+
+architecture rtl of gf_lookup_table_dp is
+  constant M      : integer := GF_POLYNOMIAL'length-1;
+
+  -- ROM table type
+  type table_t is array(0 to 2**M-1) of std_logic_vector(M-1 downto 0);
+
+  -- function to generate table contents
+  function gen_table return table_t is
+    variable index_elem : std_logic_vector(M-1 downto 0);
+    variable value_elem : std_logic_vector(M-1 downto 0);
+    variable ret        : table_t;
+  begin
+
+    if TABLE_TYPE = INV_TABLE_TYPE then
+      ret(0)        := (OTHERS => '0'); -- not defined
+      ret(1)        := std_logic_vector(to_unsigned(1,M)); -- 1 = 1*1
+
+      -- iterate over powers of primitive element
+      for i in 1 to 2**M-2 loop
+        index_elem  := prim_elem_exp(i, GF_POLYNOMIAL);
+        value_elem  := prim_elem_exp(2**M-1-i, GF_POLYNOMIAL);
+        ret(to_integer(unsigned(index_elem))) := value_elem;
+      end loop;
+
+    elsif TABLE_TYPE = LOG_TABLE_TYPE then
+      ret(0)        := (OTHERS => '0'); -- not defined
+
+      -- iterate over powers of primitive element
+      for i in 0 to 2**M-2 loop
+        index_elem  := prim_elem_exp(i, GF_POLYNOMIAL);
+        value_elem  := std_logic_vector(to_unsigned(i,M));
+        ret(to_integer(unsigned(index_elem))) := value_elem;
+      end loop;
+
+    elsif TABLE_TYPE = EXP_TABLE_TYPE then
+
+      -- iterate over powers of primitive element
+      for i in 0 to 2**M-1 loop
+        index_elem  := std_logic_vector(to_unsigned(i,M));
+        value_elem  := prim_elem_exp(i, GF_POLYNOMIAL);
+        ret(to_integer(unsigned(index_elem))) := value_elem;
+      end loop;
+    end if;
+
+    return ret;
+  end function gen_table;
+
+  constant table  : table_t := gen_table;
+
+begin
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      elem_out_a <= table(to_integer(unsigned(elem_in_a)));
+      elem_out_b <= table(to_integer(unsigned(elem_in_b)));
+    end if;
+  end process;
+end rtl;
